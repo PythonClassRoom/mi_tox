1f11eb5f1bf7dfde1d91354ecf7bb09c47023222
Erick Salas <ericksc@gmail.com>
2018-10-26 02:50:53 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mi_tox',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Erix',
+    author_email='',
+    description=''
+)
